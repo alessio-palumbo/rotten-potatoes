fc75e14625576d39d01742082d8f8eeeeb3808f3
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-10-24 15:42:07 +1100

    Add rating to movies

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -31,7 +31,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
