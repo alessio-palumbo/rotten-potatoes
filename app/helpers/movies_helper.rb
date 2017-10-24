2c0f199d020b76f9400289081d038275685f7ffa
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-24 22:13:37 +1100

    Add helper method for coloring movie ratings

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,4 +2,16 @@ module MoviesHelper
   def movie_rating(movie)
     movie.reviews.blank? ? '?' : movie.reviews.sum(&:rating) / movie.reviews.count
   end
+
+  def colorize_movie_rating(rating)
+    if rating >= 60
+      "green_rating"
+    elsif rating >= 30
+      "orange_rating"
+    elsif rating >= 0
+      "red_rating"
+    else
+      "no_rating"
+    end
+  end
 end
